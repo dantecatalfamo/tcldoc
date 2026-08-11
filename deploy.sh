206039f52b988e1842f99d251ba215ba66734170
dantecatalfamo <dante.catalfamo@gmail.com>
2026-08-10 23:15:25 -0400

    Fix typo in deploy

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-rsync -va --progress --delete --rsync-path="/usr/bin/openrsync" site/ lambda.cx:/var/www/htdocs/tcl.lambda.c/x
+rsync -va --progress --delete --rsync-path="/usr/bin/openrsync" site/ lambda.cx:/var/www/htdocs/tcl.lambda.cx/
